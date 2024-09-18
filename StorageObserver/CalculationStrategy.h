7d9cb703718007cbb784d0eb0fbab80ad4b78052
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2024-09-18 21:24:39 +0700

    Implement the strategy pattern - CalculationStrategy and ByFolderCalculationStrategy classes, created the getSize auxiliary function

---
+++ StorageObserver/CalculationStrategy.h
@@ -0,0 +1,36 @@
+#ifndef CALCULATIONSTRATEGY_H
+#define CALCULATIONSTRATEGY_H
+
+#include <QMap>
+#include <QDir>
+#include <QFileInfo>
+
+class CalculationStrategy {
+public:
+    virtual QMap<QString, qint64> calculateSize(const QString& directory) = 0;
+
+    virtual ~CalculationStrategy();
+};
+
+qint64 getSize(const QString& path) {
+    QFileInfo fileInfo(path);
+
+    if (fileInfo.isFile()) {
+        return fileInfo.size();
+    } else if (fileInfo.isDir()) {
+        QDir dir(path);
+        qint64 size = 0;
+        QFileInfoList list = dir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
+
+        for (const QFileInfo& info : list) {
+            size += getSize(info.absoluteFilePath());
+        }
+
+        return size;
+    }
+
+    return 0;
+}
+
+
+#endif // CALCULATIONSTRATEGY_H
---
+++ StorageObserver/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include <CalculationStrategy.h>
+#include <ByFolderCalculationStrategy.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
