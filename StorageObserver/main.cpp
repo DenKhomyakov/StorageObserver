6601172d0efa7276327c29a81c7ec051cc5cda3c
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2024-09-19 08:16:47 +0700

    The printResults function has been developed

--- StorageObserver/main.cpp
+++ StorageObserver/main.cpp
@@ -1,4 +1,29 @@
 #include <QCoreApplication>
+#include <QTextStream>
+
+void printResults(const QMap<QString, qint64>& results, qint64 totalSize) {
+    QTextStream out(stdout);
+
+    for (auto it = results.constBegin(); it != results.constEnd(); ++it) {
+        double precentage;
+
+        if (totalSize > 0) {
+            precentage = static_cast<double>(it.value() / totalSize * 100);
+        } else {
+            precentage = 0;
+        }
+
+        QString precentageStr;
+
+        if (precentage > 0.01) {
+            precentageStr = QString::number(precentage, 'f', 2);
+        } else {
+            precentageStr = "< 0.01";
+        }
+
+        out << it.key() << " - " << it.value() << " bytes (" << precentageStr << "%)\n";
+    }
+}
 
 int main(int argc, char *argv[])
 {
